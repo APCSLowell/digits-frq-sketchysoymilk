8545627578c75570c909681d71957486cbdef82d
sketchysoymilk <143027784+sketchysoymilk@users.noreply.github.com>
2024-04-14 18:33:29 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,27 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+		digitList = new ArrayList<Integer>();
+		int numDigits = 0;
+		int currentNum = 0;
+		while(num!=0){
+			num = num/10;
+			numDigits++;
+		}
+		for(int i = 0; i < numDigits; i++){
+			digitList.add(currentNum%10);
+			currentNum = currentNum%10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0; i<digitList.size()-1;i++){
+			if(digitList.get(i).intValue() <= digitList.get(i+1).intValue()){
+				return true;
+			}
+		}
+		return false;
 
 	}
 	
